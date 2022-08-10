9a56dcb97c13c6c25a232e0aca3e2782bdc1b803
wangwei149 <wangwei149@lenovo.com>
2022-08-10 17:45:16 +0800

    change prettier file

--- src/App.vue
+++ src/App.vue
@@ -6,22 +6,22 @@ console.log('123')
 </script>
 
 <template>
-    <div></div>
-    <HelloWorld msg="Vite + Vue" />
+  <div></div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
 .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
 }
 
 .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+  filter: drop-shadow(0 0 2em #646cffaa);
 }
 
 .logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
