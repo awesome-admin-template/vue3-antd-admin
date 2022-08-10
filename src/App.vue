4b324b9f43151b91de9aad49a8889769ae8ef00a
wangwei149 <wangwei149@lenovo.com>
2022-08-10 17:43:35 +0800

    test commit

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,7 @@ console.log('123')
 </script>
 
 <template>
-    <div>
-        <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://vuejs.org/" target="_blank">
-            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-        </a>
-    </div>
+    <div></div>
     <HelloWorld msg="Vite + Vue" />
 </template>
 
